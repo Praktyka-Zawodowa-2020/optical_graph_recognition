525ef018b644a148ac8d43f8866e8ea1e0ee4dea
Nooakk <38472314+Nooakk@users.noreply.github.com>
2020-07-20 10:55:27 +0200

    Parser comand line arguments (#15)
    Adds parsing command line arguments.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import os
 import sys
 
 import cv2 as cv
-
+import argparse
+from postprocesing import graph6_format, graphml_format
 from preprocessing import preprocess
 from segmentation import segment
 from topology_recognition import recognize_topology
-from postprocesing import graph6_format, graphml_format
+
+parser = argparse.ArgumentParser("Optical graph recognition")
+parser.add_argument("-p", "--path", help="Path to file")
+parser.add_argument("-f", "--formats", help="File format. Possible option: g6, graphml", default="graphml",
+                    choices=['graphml', 'g6'])
 
 
+# parser.add_argument("-b", "--background", help="TO DO. Choise background", default=1, choices=['1', '2'])
 def load_image(file_index):
     file_names = [
         "notebook_black_1.jpg",  # 0
@@ -29,17 +36,52 @@ def load_image(file_index):
     return source
 
 
-def main(args):
-    source = load_image(file_index=0)
+def parse_argument(file_path: str) -> str:
+    file_path.replace(" ", "")
+    if 1 < file_path.count('.') < 0:
+        print("File path is incorrect. Must be only one dot.")
+        return ''
+    head, tail = os.path.split(file_path)
+    if len(tail) == 0:
+        print("File name no exist")
+        return ''
 
+    file_name, file_ext = os.path.splitext(tail)
+    if len(file_name) == 0:
+        print("File name not found")
+        return ''
+    save_path = head + '/' + file_name
+    return save_path
+
+
+def main(args=None):
+    # source = load_image(file_index=0)
+    args = parser.parse_args()
+    file_path = args.path
+    formats = args.formats
+    save_path = parse_argument(file_path)
+
+    if len(save_path) == 0:
+        return -1
+
+    source = cv.imread(file_path)
     if source is not None:  # read successful, process image
 
-        source, binary, preprocessed = preprocess(source, True)
+        source, binary, preprocessed = preprocess(source, False)
 
-        vertices_list, visualised = segment(source, binary, preprocessed, True)
+        vertices_list, visualised = segment(source, binary, preprocessed, False)
+        if len(vertices_list) == 0:
+            print("No vertices found")
+            return -1
 
-        vertices_list = recognize_topology(vertices_list, preprocessed, visualised, True)
-
+        vertices_list = recognize_topology(vertices_list, preprocessed, visualised, False)
+        if formats == "graphml":
+            graphml_format(vertices_list, save_path)
+        elif formats == "g6":
+            graph6_format(vertices_list, save_path)
+        else:
+            print("No Format found")
+            return -1
         cv.imshow("source", source)
         # display all windows until key is pressed
         cv.waitKey(0)
@@ -49,4 +91,4 @@ def main(args):
 
 
 # if __name__ == "__main__":
-main(sys.argv[1:])
+main()
