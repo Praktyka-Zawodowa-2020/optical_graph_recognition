4cee37930e3cd3ce40a85ff85399fcfc48ad54f2
filip-chodziutko <62184852+filip-chodziutko@users.noreply.github.com>
2020-08-16 17:47:47 +0200

    Added Debuging modes (#38)
    Refactored argparser module

--- shared.py
+++ shared.py
@@ -1,14 +1,22 @@
 """Module containing global constants, functions, ..."""
-
 import numpy as np
 
 
 class Mode:
-    # Input mode indicates visual properties of given graph photo
-    GRID_BG = 1     # Hand drawn on grid/lined piece of paper (grid/lined notebook etc.)
-    CLEAN_BG = 2    # Hand drawn on empty uniform color background (on board, empty piece of paper, editor (paint))
-    PRINTED = 3     # Printed (e.g. from paper, publication, book...)
-    AUTO = 4        # Automatically chosen mode based on average background distance to objects
+    """Input mode indicates visual properties of given graph photo, see HELP below for details"""
+    HELP = '''
+        grid_bg - Hand drawn graph on grid/lined piece of paper (grid/lined notebook etc.) 
+        clean_bg - Hand drawn graph on empty uniform color background (on board, empty piece of paper, editor (paint))
+        printed - Printed graph (e.g. from paper, publication, book...)
+        auto - Mode is chosen automatically between GRID_BG and CLEAN_BG    
+    '''
+    CHOICES = ['grid_bg', 'clean_bg', 'printed', 'auto']
+    DEFAULT = 'clean_bg'
+
+    GRID_BG = CHOICES.index('grid_bg')
+    CLEAN_BG = CHOICES.index('clean_bg')
+    PRINTED = CHOICES.index('printed')
+    AUTO = CHOICES.index('auto')
 
     @staticmethod
     def get_mode(cli_arg: str):
@@ -17,25 +25,51 @@ class Mode:
         :param cli_arg: command line argument indicating Mode for processing
         :return: Mode for processing
         """
-        if cli_arg == "GRID_BG":
-            return Mode.GRID_BG
-        elif cli_arg == "CLEAN_BG":
-            return Mode.CLEAN_BG
-        elif cli_arg == "PRINTED":
-            return Mode.PRINTED
-        elif cli_arg == "AUTO":
-            return Mode.AUTO
-        else:
-            print("1: Mode \""+cli_arg+"\" is not a viable mode.")
-            return -1
+        for i in range(0, len(Mode.CHOICES)):
+            if cli_arg == Mode.CHOICES[i]:
+                return i
+
+        # invalid cli_arg value
+        print("1: Mode \""+cli_arg+"\" is not a viable mode.")
+        return -1
+
+
+class Debug:
+    """Debug mode indicates how much debugging information will be displayed"""
+    HELP = '''
+        no - no windows with debugging information are displayed
+        general - only windows with general debugging information are displayed
+        full - all windows with debugging information are displayed
+    '''
+    CHOICES = ['no', 'general', 'full']
+    DEFAULT = 'no'
+
+    NO = CHOICES.index('no')
+    GENERAL = CHOICES.index('general')
+    FULL = CHOICES.index('full')
+
+    @staticmethod
+    def get_debug(cli_arg: str):
+        """
+        Resolves debugging mode code from command line input string
+        :param cli_arg: command line argument indicating debugging mode
+        :return: debugging mode code
+        """
+        for i in range(0, len(Debug.CHOICES)):
+            if cli_arg == Debug.CHOICES[i]:
+                return i
+        # invalid cli_arg value
+        print("1: Debugging mode \""+cli_arg+"\" is not a viable one.")
+        return -1
 
 
 class Color:
-    # Logical colors
+    """Logical colors used for processing and physical used for debugging purposes"""
+    # Logical
     OBJECT = 255
     BG = 0
 
-    # Physical colors - BGR
+    # Physical (BGR)
     BLUE = (255, 0, 0)
     GREEN = (0, 255, 0)
     RED = (0, 0, 255)
@@ -47,6 +81,7 @@ class Color:
 
 
 class Kernel:
+    """Simple kernels (square matrices of ones) with different sizes used throughout processing"""
     k3 = np.ones((3, 3), dtype=np.uint8)
     k5 = np.ones((5, 5), dtype=np.uint8)
     k7 = np.ones((7, 7), dtype=np.uint8)
